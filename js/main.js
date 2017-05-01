604f75386e7e672c3748976ded1aa8c1d38ee8d0
lauralee51 <lauralee51@gmail.com>
2017-04-30 22:59:15 -0400

    open map is working

--- js/main.js
+++ js/main.js
@@ -6,56 +6,70 @@
    HTML structures and invoke the proper JS code accordingly. Be sure
    to reference the Bootstrap documentation.)
 */
- $(document).ready(function(){
-   
-  // Inside of your on ready handler, invoke the Leaflet.js library
-  // to draw a map in your `#map-container` div.
-   
-   
-
-var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
-var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 18, attribution: osmAttrib});
-   
-  // Add 2 layers to your map you have visuals. Use the Open Street Maps
-  // tiles served through the MapQuest CDN. Consult this example to set up
-  // the map tiles layers.
-   
-var satLayer = L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.png', {
+$(document).ready(function() { 
+  
+// TODO: Inside of your on ready handler, invoke the Leaflet.js library
+// to draw a map in your `#map-container` div.
+var map = L.map('map-container').setView([46.8523, -121.7603], 13);
+  
+// Create map
+  
+var drawLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+  
+// Add layer to map    
+  
+var terrainLayer = L.tileLayer('http://tile.stamen.com/terrain/{z}/{x}/{y}.jpg', {
     attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
-    subdomains: ['otile1','otile2','otile3','otile4']
-});
+}).addTo(map);
+  
 
-var drawLayer = L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
-    subdomains: ['otile1','otile2','otile3','otile4']
-});
-
+ // map layers selection box
+  
 var mapLayers = {
-    "Satellite": satLayer,
+    "Terrain": terrainLayer,
     "Map View": drawLayer,
-    "Open Street Maps": osm
+   
 }
 
-var map = L.map('map-container').setView([46.852, -121.760], 13);
-
+// controller for map selection
 L.control.layers(mapLayers).addTo(map);
-satLayer.addTo(map);
+terrainLayer.addTo(map); 
+
+
+  
+//Points of Interest Markers//  
+
+L.marker([46.8523, -121.76403]).addTo(map)
+    .bindPopup('<b>Welcome to Mt. Rainier!</b><br>The mountain is 14,111 feet high.')
+    .openPopup();
+  
+L.marker([46.871, -121.732]).addTo(map)
+  .bindPopup("<b>Steamboat Prow</b><br>Hike to this sub-peak and ski the Inter Glacier.")
+  .openPopup();
+  
+L.marker([46.902, -121.643]).addTo(map)
+  .bindPopup("<b>White River Campground</b><br>Bring your tent and relax or do some hiking.")
+  .openPopup();
+
+         
+});
+
    
   // Customize that Map to show markers with popups at no fewer than 3
   // interesting locations. 
+/*   
+L.marker([46.852, -121.760]).addTo(map)
+bindPopup("<b>Welcome to Mt. Rainier!</b><br>This peak is 4,392 feet high.")
+openPopup();
+
+L.marker([46.871, -121.732]).addTo(map)
+bindPopup("<b>Steamboat Prow</b><br>Hike to this sub-peak and ski the Inter Glacier.")
+openPopup();
    
-var marker = L.marker([46.852, -121.760]).addTo(map);
-marker.bindPopup("<b>Welcome to Mt. Rainier!</b><br>This peak is 4,392 feet high.");
-
-var marker1 = L.marker([46.871, -121.732]).addTo(map);
-marker.bindPopup("<b>Steamboat Prow</b><br>Hike to this sub-peak and ski the Inter Glacier.");
-   
-var marker2 = L.marker([46.902, -121.643]).addTo(map);
-marker.bindPopup("<b>White River Campground</b><br>Bring your tent and relax or do some hiking.");   
-  
- });
-
-  
-  
-
+L.marker([46.902, -121.643]).addTo(map)
+bindPopup("<b>White River Campground</b><br>Bring your tent and relax or do some hiking.")  
+openPopup();  
+ 
+ });*/
