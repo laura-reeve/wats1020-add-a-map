b534ad25b238cc38a2390b119332aa7609a57c54
lauralee51 <lauralee51@gmail.com>
2017-04-30 21:59:29 -0400

    assignment completed - open map works

--- js/main.js
+++ js/main.js
@@ -7,8 +7,11 @@
    to reference the Bootstrap documentation.)
 */
  $(document).ready(function(){
+   
   // Inside of your on ready handler, invoke the Leaflet.js library
   // to draw a map in your `#map-container` div.
+   
+   
 
 var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
@@ -17,6 +20,7 @@ var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 18, attribution: osmAttr
   // Add 2 layers to your map you have visuals. Use the Open Street Maps
   // tiles served through the MapQuest CDN. Consult this example to set up
   // the map tiles layers.
+   
 var satLayer = L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
     subdomains: ['otile1','otile2','otile3','otile4']
@@ -38,13 +42,19 @@ var map = L.map('map-container').setView([46.852, -121.760], 13);
 L.control.layers(mapLayers).addTo(map);
 satLayer.addTo(map);
    
-  // TODO: Customize that Map to show markers with popups at no fewer than 3
-  // interesting locations. (You'll need to figure out the latitude/longitude for
-  // these locations using a mapping tool such as Google Maps.)
+  // Customize that Map to show markers with popups at no fewer than 3
+  // interesting locations. 
+   
 var marker = L.marker([46.852, -121.760]).addTo(map);
 marker.bindPopup("<b>Welcome to Mt. Rainier!</b><br>This peak is 4,392 feet high.");
 
-  });
+var marker1 = L.marker([46.871, -121.732]).addTo(map);
+marker.bindPopup("<b>Steamboat Prow</b><br>Hike to this sub-peak and ski the Inter Glacier.");
+   
+var marker2 = L.marker([46.902, -121.643]).addTo(map);
+marker.bindPopup("<b>White River Campground</b><br>Bring your tent and relax or do some hiking.");   
+  
+ });
 
   
   
